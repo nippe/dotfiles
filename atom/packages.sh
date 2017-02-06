060f32650807f8db97029e93d2c5f3c927523778
Niklas Nihlén <niklas.nihlen@acast.com>
2017-02-06 22:14:22 +0100

    Added atom packages

---
+++ atom/packages.sh
@@ -0,0 +1,10 @@
+apm install file-icons
+apm install atom-beautify
+apm install linter 
+apm install vim-mode-plus
+apm install highlight-selected
+#apm install language-mustache
+apm install stylus
+apm install language-docker
+apm install git-plus
+apm install terminal-plus
