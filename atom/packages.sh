54fbc793ac1296b03d66af1a87d77b4d8803100f
Niklas Nihlén <niklas.nihlen@expressen.se>
2017-03-14 15:14:38 +0100

    installing more atom pacakages

--- atom/packages.sh
+++ atom/packages.sh
@@ -1,6 +1,8 @@
 apm install file-icons
 apm install atom-beautify
 apm install linter 
+apm install linter-eslint 
+apm install linter-ui-default
 apm install vim-mode-plus
 apm install highlight-selected
 #apm install language-mustache
@@ -9,3 +11,8 @@ apm install language-docker
 apm install git-plus
 apm install terminal-plus
 apm install atom-beautify
+apm install editorconfig
+apm install language-docker
+apm install wakatime
+apm install ramda-syntax
+
